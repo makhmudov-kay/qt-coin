441b5552d0c2b408ef5278ac4be3e3313ed7e5a7
Lenovo <makhmudov.kay@gmail.com>
2022-03-28 08:17:38 +0500

    responsive header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ru_RU } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* NG-ZORRO */
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzBackTopModule } from 'ng-zorro-antd/back-top';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
@@ -10,9 +36,29 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    /* NG-ZORRO */
+    NzLayoutModule,
+    NzGridModule,
+    NzMenuModule,
+    NzIconModule,
+    NzButtonModule,
+    NzTypographyModule,
+    NzFormModule,
+    NzInputModule,
+    NzSliderModule,
+    NzCheckboxModule,
+    NzCardModule,
+    NzCollapseModule,
+    NzModalModule,
+    NzSpinModule,
+    NzBackTopModule,
+    NzDividerModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: ru_RU }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
